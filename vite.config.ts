667c326af8ecc93740bcc05c2ef9d2a4bd747a79
rainatsai <yuting092142@gmail.com>
2025-07-16 14:47:00 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/raina-tsai/', // raina/
+  plugins: [react()],
+})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
