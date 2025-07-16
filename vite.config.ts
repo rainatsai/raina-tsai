9a0576487e8542f8c2795062501cd5bf4b298314
rainatsai <yuting092142@gmail.com>
2025-07-16 11:10:01 +0000

    Initial commit for personal website

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // <--- This line belongs HERE, in vite.config.ts!
 
-export default defineConfig({
-  base: '/raina-tsai/',
-  plugins: [react()],
-})
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
